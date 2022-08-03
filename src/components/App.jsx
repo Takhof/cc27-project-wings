33329d73063856eff5954e5870e5fe422bf63dc8
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-03 22:51:38 +0900

    Test: Update App.jsx to map data to render in User component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
+import User from "./User";
+import "../styles.css";
 
 const URL = "http://localhost:3030";
 
 function App() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState("");
 
+  // TESTING DATA FROM BACKEND
   const fetchResponse = async (e) => {
     const res = await fetch(`${URL}/users`, { method: "GET" });
     const data = await res.json();
-    console.log(data);
+    setUsers(data);
+    // TESTING
+    console.log("Users", users);
   };
 
+  // populate Users when users variable is true
   return (
     <div>
       <header>
         <h1>Welcome to Wings 🦋</h1>
-        <button onClick={fetchResponse}>Talk to Server</button>
-        <p>{users ? users : "Placeholder"}</p>
+        <button className="btn" onClick={fetchResponse}>
+          Show me the users!
+        </button>
+        {users && (
+          <div className="test-container">
+            {users.map((user) => {
+              return <User email={user.email} pswd={user.password} />;
+            })}
+          </div>
+        )}
       </header>
     </div>
   );

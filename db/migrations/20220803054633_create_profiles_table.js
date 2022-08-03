b43b06d60fecbd10976830d58c6b2d4383f3a3a7
Zach Brown <zach@zach.tokyo>
2022-08-03 16:52:30 +0900

    Fix: Correct db migration file

--- db/migrations/20220803054633_create_profiles_table.js
+++ db/migrations/20220803054633_create_profiles_table.js
@@ -2,21 +2,8 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema
-  .createTable("posts", function (table) {
-    table.increments("post_id").primary(); // Set this column as the primary key
-    table
-      .integer("user_id")
-      .notNullable();
-//      .references("profiles.user_id");
-    table.string("post_text",500);
-    table.string("post_photo",255);
-    table
-      .dateTime("post_date")
-      .notNullable()
-      .defaultTo(knex.fn.now());
-  });
+ exports.up = function(knex) {
+  
 };
 
 /**
@@ -24,5 +11,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  knex.schema.dropTable("posts");
+  
 };

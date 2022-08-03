c7e519df93cf8326e0b75ee4b92ea0d1cf0defc7
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-03 16:50:00 +0900

    Chore: Add API endpoints

--- src/server.js
+++ src/server.js
@@ -1,31 +1,64 @@
 const express = require("express");
 const cors = require("cors");
 // const path = require("path");
-
 const app = express();
-
 const PORT = process.env.PORT || 3030;
 
-// TODO how to connect React & Express...
+// *************************************************** MIDDLEWARE ***********************************************************
 
-// MIDDLEWARE
-// // To allow requests using localhost
+// Required for front and backend using localhost
 app.use(cors());
-// Parsing JSON
+// Parse JSON
 app.use(express.json());
 // Parsing form-encoded data
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(201).send(JSON.stringify("This is from the server!"));
+// *************************************************** GET REQUESTS ***********************************************************
+
+// LOGIN
+app.get("/api/login", (req, res) => {
+  // call model to query db (users)
+  // re-route to /api/newsfeed?
 });
 
-app.post("/", (req, res) => {
-  console.log("received request");
-  res.send("Thanks for the request");
+// VIEW NEWSFEED
+app.get("/api/newsfeed", (req, res) => {
+  // call model to query db
+  // send newsfeed object to frontend
 });
 
-// Spool up
+// DIRECTORY
+app.get("/api/users", (req, res) => {
+  // call model to query db (users)
+  // send profile object to frontend
+});
+
+// SINGLE PROFILE (called on click in directory)
+app.get("/api/profile/:id", (req, res) => {
+  // call model to query db (profiles)
+  // send profile object to frontend
+});
+
+// *************************************************** POST REQUESTS ***********************************************************
+
+// REGISTER
+app.post("/api/register", (req, res) => {
+  // call model to update db (profiles)
+  // re-route to /api/newsfeed?
+});
+
+// UPDATE PROFILE
+app.post("/api/profile", (req, res) => {
+  // call model to update db (profiles)
+  // return profile object to frontend
+});
+
+// ADD POST
+app.post("/api/post", (req, res) => {
+  // call model to update db (posts)
+  // send newsfeed object to frontend
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });

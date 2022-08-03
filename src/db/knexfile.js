519666007ce380832e3b91a80fc4b352ab19571b
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-03 14:25:44 +0900

    Chore: Add user to knex config

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,29 +1,28 @@
 module.exports = {
-  development: {
-    client: "pg",
-    connection: {
-      database: "wings",
-      // TODO add user...
-      user: "",
-      password: null,
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: { directory: "./data/seeds" },
-  },
+	development: {
+		client: "pg",
+		connection: {
+			database: "wings",
+			user: "wings",
+			password: null,
+		},
+		migrations: {
+			directory: "./data/migrations",
+		},
+		seeds: { directory: "./data/seeds" },
+	},
 
-  production: {
-    client: "pg",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
+	production: {
+		client: "pg",
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: { rejectUnauthorized: false },
+		},
+		migrations: {
+			directory: "./data/migrations",
+		},
+		seeds: {
+			directory: "./data/seeds",
+		},
+	},
 };

b9e88d4944c2525799b1acbc110a485ac4f45970
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-04 20:29:32 +0900

    Chore: Update seed files to reset sequence with each re-seed

--- db/seeds/users_seed.js
+++ db/seeds/users_seed.js
@@ -3,18 +3,66 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-	// Deletes ALL existing entries
-	await knex("users").del();
-	await knex("users").insert([
-		{ user_id: 2, email: "aki@yahoo.co.jp", password: "Passw0rd", active: true },
-		{ user_id: 3, email: "booyah@aol.com", password: "Passw0rd", active: true },
-		{ user_id: 4, email: "shin1997@hotmail.com", password: "Passw0rd", active: true },
-		{ user_id: 5, email: "oliver_daiba@gmail.com", password: "Passw0rd", active: true },
-		{ user_id: 6, email: "mickey@disney.com", password: "Passw0rd", active: true },
-		{ user_id: 7, email: "hara_juku@gmail.com", password: "Passw0rd", active: true },
-		{ user_id: 8, email: "skytree@yahoo.co.jp", password: "Passw0rd", active: true },
-		{ user_id: 9, email: "ekb48@freemail.jp", password: "Passw0rd", active: true },
-		{ user_id: 10, email: "mumu@yahoo.com", password: "Passw0rd", active: true },
-		{ user_id: 11,email: "yoko.hama@codechrysalis.io", password: "Passw0rd", active: true },
-	]);
+  // Deletes ALL existing entries
+  await knex("users").del();
+
+  await knex.raw("ALTER SEQUENCE users_user_id_seq RESTART WITH 1");
+
+  await knex("users").insert([
+    {
+      user_id: 2,
+      email: "aki@yahoo.co.jp",
+      password: "Passw0rd",
+      active: true,
+    },
+    { user_id: 3, email: "booyah@aol.com", password: "Passw0rd", active: true },
+    {
+      user_id: 4,
+      email: "shin1997@hotmail.com",
+      password: "Passw0rd",
+      active: true,
+    },
+    {
+      user_id: 5,
+      email: "oliver_daiba@gmail.com",
+      password: "Passw0rd",
+      active: true,
+    },
+    {
+      user_id: 6,
+      email: "mickey@disney.com",
+      password: "Passw0rd",
+      active: true,
+    },
+    {
+      user_id: 7,
+      email: "hara_juku@gmail.com",
+      password: "Passw0rd",
+      active: true,
+    },
+    {
+      user_id: 8,
+      email: "skytree@yahoo.co.jp",
+      password: "Passw0rd",
+      active: true,
+    },
+    {
+      user_id: 9,
+      email: "ekb48@freemail.jp",
+      password: "Passw0rd",
+      active: true,
+    },
+    {
+      user_id: 10,
+      email: "mumu@yahoo.com",
+      password: "Passw0rd",
+      active: true,
+    },
+    {
+      user_id: 11,
+      email: "yoko.hama@codechrysalis.io",
+      password: "Passw0rd",
+      active: true,
+    },
+  ]);
 };

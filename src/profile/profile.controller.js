062564ef57140ea3fca51c200612375dcb4d3f17
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-04 19:12:43 +0900

    Feat: Complete getByID for profiles

--- src/profile/profile.controller.js
+++ src/profile/profile.controller.js
@@ -1,13 +1,17 @@
 const profileModel = require("./profile.model");
 
 module.exports = {
+  // DONE
   index: async function (req, res) {
     const profiles = await profileModel.getAll();
     res.send(profiles);
   },
 
+  // VIEW SINGLE PROFILE
   view: async function (req, res) {
-    // TODO
+    const id = parseInt(req.params.id);
+    const profile = await profileModel.getByID(id);
+    res.send(profile);
   },
 
   edit: async function (req, res) {

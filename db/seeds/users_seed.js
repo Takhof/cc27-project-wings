d0b1694bb88954d4f0644ffcdce5edb7064cbf33
Zach Brown <zach@zach.tokyo>
2022-08-04 11:33:02 +0900

    Chore: add db seed for posts

--- db/seeds/users_seed.js
+++ db/seeds/users_seed.js
@@ -6,15 +6,15 @@ exports.seed = async function (knex) {
 	// Deletes ALL existing entries
 	await knex("users").del();
 	await knex("users").insert([
-		{ email: "aki@yahoo.co.jp", password: "Passw0rd", active: true },
-		{ email: "booyah@aol.com", password: "Passw0rd", active: true },
-		{ email: "shin1997@hotmail.com", password: "Passw0rd", active: true },
-		{ email: "oliver_daiba@gmail.com", password: "Passw0rd", active: true },
-		{ email: "mickey@disney.com", password: "Passw0rd", active: true },
-		{ email: "hara_juku@gmail.com", password: "Passw0rd", active: true },
-		{ email: "skytree@yahoo.co.jp", password: "Passw0rd", active: true },
-		{ email: "ekb48@freemail.jp", password: "Passw0rd", active: true },
-		{ email: "mumu@yahoo.com", password: "Passw0rd", active: true },
-		{ email: "yoko.hama@codechrysalis.io", password: "Passw0rd", active: true },
+		{ user_id: 2, email: "aki@yahoo.co.jp", password: "Passw0rd", active: true },
+		{ user_id: 3, email: "booyah@aol.com", password: "Passw0rd", active: true },
+		{ user_id: 4, email: "shin1997@hotmail.com", password: "Passw0rd", active: true },
+		{ user_id: 5, email: "oliver_daiba@gmail.com", password: "Passw0rd", active: true },
+		{ user_id: 6, email: "mickey@disney.com", password: "Passw0rd", active: true },
+		{ user_id: 7, email: "hara_juku@gmail.com", password: "Passw0rd", active: true },
+		{ user_id: 8, email: "skytree@yahoo.co.jp", password: "Passw0rd", active: true },
+		{ user_id: 9, email: "ekb48@freemail.jp", password: "Passw0rd", active: true },
+		{ user_id: 10, email: "mumu@yahoo.com", password: "Passw0rd", active: true },
+		{ user_id: 11,email: "yoko.hama@codechrysalis.io", password: "Passw0rd", active: true },
 	]);
 };

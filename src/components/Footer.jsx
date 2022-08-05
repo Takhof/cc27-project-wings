d6290a5e8a8db181406db3fc6d883fc326feb93c
Zach Brown <zach@zach.tokyo>
2022-08-05 23:01:07 +0900

    Fix: Correct links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,10 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-links-container">
-        <a href="/Login">Login</a>
+        <a href="/">Login</a>
         <a href="/Register">Sign up</a>
-        <a href="/CreateProfile">Create Profile</a>
+        <a href="/CreateEditProfile">Create/Edit Profile</a>
+				<a href="/SingleProfileView">Single Profile View</a>
         <a href="/Directory">Directory</a>
         <a href="/Contact"> Contact us</a>
         <small className="copyright"> Wings © 2022 Jozajo International </small>

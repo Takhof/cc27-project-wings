cb83a35eca78dfe674ef215f31dfcfb2ef368316
Zach Brown <zach@zach.tokyo>
2022-08-05 23:21:30 +0900

    Feat: Create post component and add to newsfeed

--- src/components/Newsfeed.jsx
+++ src/components/Newsfeed.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Post from "./Post"
 
 function Newsfeed() {
 	return (
 		<div>
       <h3>Newsfeed Component</h3>
+      <Post />
 		</div>
 	);
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Post() {
+	return (
+		<div>
+      <h3>Post Component</h3>
+		</div>
+	);
+}
+
+export default Post;

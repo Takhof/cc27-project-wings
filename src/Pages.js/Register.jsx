acf9a72f5e005a14a3319fb34ef03a6d4ea67748
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-06 08:00:56 +0900

    Fix:Add forHtml to jsx

--- src/Pages.js/Register.jsx
+++ src/Pages.js/Register.jsx
@@ -6,79 +6,79 @@ import "../styles.css";
 
 const URL = "http://localhost:3030";
 function Register() {
-  const [formData, setFormData] = useState({});
+	const [formData, setFormData] = useState({});
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setFormData((values) => ({ ...values, [name]: value }));
-  };
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setFormData((values) => ({ ...values, [name]: value }));
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    const options = {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+		const options = {
+			method: "POST",
+			body: JSON.stringify(formData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-    const res = await fetch(`${URL}/users/save`, options);
-    const data = await res.json();
-    console.log("Data from Server", data);
-    // TODO Redirect user to profile creation page
-  };
-  return (
-    <div>
-      <Header />
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <h2 className="form-header">Sign Up</h2>
-          <label className="form-label" for="email">
-            Email<span>*</span>
-          </label>
-          <input
-            className="form-input"
-            type="email"
-            name="email"
-            value={formData.email || ""}
-            onChange={handleChange}
-            required
-          />
-          <label className="form-label" for="password">
-            Password<span>*</span>
-          </label>
-          <input
-            className="form-input"
-            type="password"
-            name="password"
-            value={formData.password || ""}
-            onChange={handleChange}
-            required
-          />
-          <label className="form-label" for="password">
-            Confirm Password<span>*</span>
-          </label>
-          <input
-            className="form-input"
-            type="password"
-            name="password"
-            value={formData.password || ""}
-            onChange={handleChange}
-            required
-          />
-          <input className="btn" type="submit" />
-          <span className="links">
-            <a className="register-link" href="/">
-              Already have an account? Click here to login
-            </a>
-          </span>
-        </form>
-      </div>
-      <Footer />
-    </div>
-  );
+		const res = await fetch(`${URL}/users/save`, options);
+		const data = await res.json();
+		console.log("Data from Server", data);
+		// TODO Redirect user to profile creation page
+	};
+	return (
+		<div>
+			<Header />
+			<div className="form-container">
+				<form onSubmit={handleSubmit}>
+					<h2 className="form-header">Sign Up</h2>
+					<label className="form-label" htmlFor="email">
+						Email<span>*</span>
+					</label>
+					<input
+						className="form-input"
+						type="email"
+						name="email"
+						value={formData.email || ""}
+						onChange={handleChange}
+						required
+					/>
+					<label className="form-label" htmlFor="password">
+						Password<span>*</span>
+					</label>
+					<input
+						className="form-input"
+						type="password"
+						name="password"
+						value={formData.password || ""}
+						onChange={handleChange}
+						required
+					/>
+					<label className="form-label" htmlFor="password">
+						Confirm Password<span>*</span>
+					</label>
+					<input
+						className="form-input"
+						type="password"
+						name="password"
+						value={formData.password || ""}
+						onChange={handleChange}
+						required
+					/>
+					<input className="btn" type="submit" />
+					<span className="links">
+						<a className="register-link" href="/">
+							Already have an account? Click here to login
+						</a>
+					</span>
+				</form>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 export default Register;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,73 +3,73 @@ import { useState } from "react";
 import "../styles.css";
 
 function Login({ setLoggedInUser }) {
-  const [formData, setFormData] = useState({});
+	const [formData, setFormData] = useState({});
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setFormData((values) => ({ ...values, [name]: value }));
-  };
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setFormData((values) => ({ ...values, [name]: value }));
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    const options = {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+		const options = {
+			method: "POST",
+			body: JSON.stringify(formData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-    const res = await fetch("/users/login", options);
-    const data = await res.json();
+		const res = await fetch("/users/login", options);
+		const data = await res.json();
 
-    setLoggedInUser(data);
+		setLoggedInUser(data);
 
-    // TODO Redirect user to profile creation page
-    console.log("Data from Server", data);
-  };
+		// TODO Redirect user to profile creation page
+		console.log("Data from Server", data);
+	};
 
-  return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <h2 className="form-header">Login</h2>
-        <div className="form-item">
-          <label className="form-label" for="email">
-            Email<span>*</span>
-          </label>
-          <input
-            className="form-input"
-            type="email"
-            name="email"
-            value={formData.email || ""}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-item">
-          <label className="form-label" for="password">
-            Password<span>*</span>
-          </label>
-          <input
-            className="form-input"
-            type="password"
-            name="password"
-            value={formData.password || ""}
-            onChange={handleChange}
-            required
-          />{" "}
-        </div>
-        <input className="btn" type="submit" />
-        <span className="links">
-          <a className="register-link" href="/Register">
-            First time user? Sign up here
-          </a>
-        </span>
-      </form>
-    </div>
-  );
+	return (
+		<div className="form-container">
+			<form onSubmit={handleSubmit}>
+				<h2 className="form-header">Login</h2>
+				<div className="form-item">
+					<label className="form-label" htmlFor="email">
+						Email<span>*</span>
+					</label>
+					<input
+						className="form-input"
+						type="email"
+						name="email"
+						value={formData.email || ""}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div className="form-item">
+					<label className="form-label" htmlFor="password">
+						Password<span>*</span>
+					</label>
+					<input
+						className="form-input"
+						type="password"
+						name="password"
+						value={formData.password || ""}
+						onChange={handleChange}
+						required
+					/>{" "}
+				</div>
+				<input className="btn" type="submit" />
+				<span className="links">
+					<a className="register-link" href="/Register">
+						First time user? Sign up here
+					</a>
+				</span>
+			</form>
+		</div>
+	);
 }
 
 export default Login;

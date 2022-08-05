2d1859967815446a115e58beb0ea98f74444e5d2
Zach Brown <zach@zach.tokyo>
2022-08-05 20:01:55 +0900

    Style: Delete unused references and old comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import User from "./User";
 import "../styles.css";
 import Login from "./Login";
 import Header from "./Header";
@@ -17,35 +16,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   const [users, setUsers] = useState("");
-
-//   // TESTING DATA FROM BACKEND
-//   const fetchResponse = async (e) => {
-//     const res = await fetch(`${URL}/users`, { method: "GET" });
-//     const data = await res.json();
-//     setUsers(data);
-//     // TESTING
-//     console.log("Users", users);
-//   };
-
-//   // populate Users when users variable is true
-//   return (
-//     <div>
-//       <header>
-//         <h1>Welcome to Wings 🦋</h1>
-//         <button className="btn" onClick={fetchResponse}>
-//           Show me the users!
-//         </button>
-//         {users && (
-//           <div className="test-container">
-//             {users.map((user) => {
-//               return <User email={user.email} pswd={user.password} />;
-//             })}
-//           </div>
-//         )}
-//       </header>
-//     </div>
-//   );
-// }

2040b1ec190a8a2e09251f8018ded831be8231de
Zach Brown <zach@zach.tokyo>
2022-08-06 14:45:03 +0900

    Feat: Profile component displays profile from database

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Profile(userId) {
+	const [profile, setProfile] = useState();
+
+	const getProfileData = async () => {
+		const res = await fetch(`/profiles/view/${userId.userId}`, { method: "GET" });
+		const data = await res.json();
+
+		setProfile(data);
+		console.log("Profile:", profile)
+	};
+
+	useEffect(() => {
+		getProfileData();
+	}, []);
 
 	return (
 		<div>
-      <h3>Profile Component</h3>
-			Profile for userID: {userId.userId}
+      <h3>Profile</h3>
+			<img src={profile.photoURL}></img>
+			<p>Name: <span>{profile.fullName}</span></p>
+			<p>Email: <span>{profile.email}</span></p>
+			<p>About: <span>{profile.about}</span></p>
+			<p>Linkedin URL: <span>{profile.linkedIn}</span></p>
+			<p>Twitter Handle: <span>{profile.twitter}</span></p>
+			<p>Cohort: <span>{profile.cohort && "CC" + profile.cohort}</span></p>
 		</div>
 	);
 }

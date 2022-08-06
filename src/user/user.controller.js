2196b023983c5d4b1e640dda2d568cc267191865
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-06 14:10:22 +0900

    Fix: Pick up user data as object from API for login

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,37 +1,37 @@
 const userModel = require("./user.model");
 
 module.exports = {
-	save: async function (req, res) {
-		// id auto-generated
-		const { email, password } = req.body;
+  save: async function (req, res) {
+    // id auto-generated
+    const { email, password } = req.body;
 
-		const payload = {
-			email,
-			password,
-		};
+    const payload = {
+      email,
+      password,
+    };
 
-		let user;
+    let user;
 
-		user = await userModel.create(payload);
-		res.send(user);
-	},
+    user = await userModel.create(payload);
+    res.send(user);
+  },
 
-	login: async function (req, res) {
-		const { email, password } = req.body;
+  login: async function (req, res) {
+    const { email, password } = req.body;
 
-		let user;
+    let user;
 
-		user = await userModel.login(email, password);
-		if (user.length === 0){
-			res.send(JSON.stringify("Invalid credentials"));
-		} else {
-			res.send(user);
-		}
-	},
+    user = await userModel.login(email, password);
+    if (user.length === 0) {
+      res.send(JSON.stringify("Invalid credentials"));
+    } else {
+      res.send(user[0]);
+    }
+  },
 
-	// ***** DEVELOPMENT ONLY *****
-	index: async function (req, res) {
-		const users = await userModel.getAll();
-		res.send(users);
-	},
+  // ***** DEVELOPMENT ONLY *****
+  index: async function (req, res) {
+    const users = await userModel.getAll();
+    res.send(users);
+  },
 };

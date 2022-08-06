5f031fe6473645dc4beeebc4285ec9f4d57a0432
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-06 10:49:43 +0900

    Feat: Add card view to directory

--- src/Pages.js/Directory.jsx
+++ src/Pages.js/Directory.jsx
@@ -7,30 +7,37 @@ function Directory() {
 	const URL = "http://localhost:3030";
 	const [profiles, setProfiles] = useState("");
 
-  // TESTING DATA FROM BACKEND
-  const fetchResponse = async (e) => {
-    const res = await fetch(`${URL}/profiles`, { method: "GET" });
-    const data = await res.json();
-    setProfiles(data);
-    // TESTING
-    console.log("profiles: ", profiles);
-  };
+	// TESTING DATA FROM BACKEND
+	const fetchResponse = async (e) => {
+		const res = await fetch(`${URL}/profiles`, { method: "GET" });
+		const data = await res.json();
+		setProfiles(data);
+		// TESTING
+		console.log("profiles: ", profiles);
+	};
 
 	return (
 		<div>
 			<Header />
-			<h2>Directory </h2>
+			<h2 className="Directory">Directory </h2>
 			<div>
-         <button className="btn" onClick={fetchResponse}>
-           Load profile directory
-         </button>
-				 {profiles && (
-          <div className="test-container">
-            {profiles.map((profile) => {
-              return  <ProfileSummary userId={profile.userId} photoURL={profile.photoURL} fullName={profile.fullName} cohort={profile.cohort}/>
-            })}
+				<button className="btn" onClick={fetchResponse}>
+					Load profile directory
+				</button>
+				{profiles && (
+					<div className="test-container">
+						{profiles.map((profile) => {
+							return (
+								<ProfileSummary
+									userId={profile.userId}
+									photoURL={profile.photoURL}
+									fullName={profile.fullName}
+									cohort={profile.cohort}
+								/>
+							);
+						})}
 					</div>
-				 )}
+				)}
 			</div>
 			<Footer />
 		</div>
--- src/components/ProfileSummary.jsx
+++ src/components/ProfileSummary.jsx
@@ -6,11 +6,10 @@ function ProfileSummary({ userId, photoURL, fullName, cohort }) {
 		<div>
 			{/* <h3>Profile Summary Component</h3> */}
 			<div className="Picture">
-				<h3>{fullName}</h3>
-
 				<a href={"/SingleProfileView?user_id=" + userId}>
 					<img className="img" src={photoURL}></img>
 				</a>
+				<h3>{fullName}</h3>
 				<h4>Cohort: CC{cohort}</h4>
 			</div>
 		</div>

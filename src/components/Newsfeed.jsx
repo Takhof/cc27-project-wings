4cf4b9e9ff89ce61d86491f0c6a0a723eeaf2d72
Zach Brown <zach@zach.tokyo>
2022-08-06 11:21:51 +0900

    Feat: Newsfeed displays posts from database

--- src/components/Newsfeed.jsx
+++ src/components/Newsfeed.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post"
 
 function Newsfeed() {
+	const URL = "http://localhost:3030";
+	const [posts, setPosts] = useState("");
+
+  const fetchResponse = async (e) => {
+    const res = await fetch(`${URL}/posts`, { method: "GET" });
+    const data = await res.json();
+    setPosts(data);
+  };
 	return (
 		<div>
       <h3>Newsfeed Component</h3>
-      <Post />
+			<div>
+         <button className="btn" onClick={fetchResponse}>
+           Load posts
+         </button>
+				 {posts && (
+						<div>
+							{posts.map((post) => {
+								return  <Post userId={post.userID} profilePhoto={post.profilePhoto} fullName={post.fullName} text={post.text} date={post.date}/>
+							})}
+						</div>
+					)}
+			</div>
 		</div>
 	);
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-function Post() {
+function Post({userId, profilePhoto, fullName, text, date}) {
 	return (
 		<div>
       <h3>Post Component</h3>
+			<div>
+				<div>
+					<a href={"/SingleProfileView?user_id=" + userId}><img src={profilePhoto}></img></a>
+					{fullName}
+					{date}
+				</div>
+				<div>
+					{text}
+				</div>
+			</div>
 		</div>
 	);
 }

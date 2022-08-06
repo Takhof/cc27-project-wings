dea9d2f5cec5ccea30fc1c4d61425ee16c489aa0
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-06 16:48:26 +0900

    Feat: Add CSS Grid to SPV

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,36 +1,40 @@
 import React, { useEffect, useState } from "react";
+import "../styles.css";
 
 function Profile(userId) {
-	const [profile, setProfile] = useState();
+  const [profile, setProfile] = useState();
 
-	const getProfileData = async () => {
-		const res = await fetch(`/profiles/view/${userId.userId}`, { method: "GET" });
-		const data = await res.json();
+  const getProfileData = async () => {
+    const res = await fetch(`/profiles/view/${userId.userId}`, {
+      method: "GET",
+    });
+    const data = await res.json();
 
-		setProfile(data);
-		console.log("Profile:", profile)
-	};
+    setProfile(data);
+    console.log("Profile:", profile);
+  };
 
-	useEffect(() => {
-		getProfileData();
-	}, []);
+  useEffect(() => {
+    getProfileData();
+  }, []);
 
-	return (
-		<div>
+  return (
+    <div>
       <h3>Profile</h3>
-			{profile && (
-				<div>
-					<img src={profile.photoURL}></img>
-					<p>Name: <span>{profile.fullName}</span></p>
-					<p>Email: <span>{profile.email}</span></p>
-					<p>About: <span>{profile.about}</span></p>
-					<p>Linkedin URL: <span>{profile.linkedIn}</span></p>
-					<p>Twitter Handle: <span>{profile.twitter}</span></p>
-					<p>CC Cohort: <span>{profile.cohort && "CC" + profile.cohort}</span></p>
-				</div>
-			)};
-		</div>
-	);
+      {profile && (
+        <div>
+          <img src={profile.photoURL}></img>
+          <p>Name: {profile.fullName}</p>
+          <p>Email: {profile.email}</p>
+          <p>About: {profile.about}</p>
+          <p>Linkedin URL: {profile.linkedIn}</p>
+          <p>Twitter Handle: {profile.twitter}</p>
+          <p>CC Cohort: {profile.cohort && "CC" + profile.cohort}</p>
+        </div>
+      )}
+      ;
+    </div>
+  );
 }
 
 export default Profile;

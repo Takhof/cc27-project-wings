8dfc230a759251c6ad4cdb62524b2e8805c1a757
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-06 10:34:01 +0900

    Feat: Add change to directory view

--- src/components/ProfileSummary.jsx
+++ src/components/ProfileSummary.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import "../styles.css";
 
-function ProfileSummary({userId, photoURL, fullName, cohort}) {
+function ProfileSummary({ userId, photoURL, fullName, cohort }) {
 	return (
 		<div>
-      <h3>Profile Summary Component</h3>
-      <div>
-      <a href={"/SingleProfileView?user_id=" + userId}><img src={photoURL}></img></a>
-      <h3>{fullName}</h3>
-      <h4>Cohort: CC{cohort}</h4>
-      </div>
+			{/* <h3>Profile Summary Component</h3> */}
+			<div className="Picture">
+				<h3>{fullName}</h3>
 
+				<a href={"/SingleProfileView?user_id=" + userId}>
+					<img className="img" src={photoURL}></img>
+				</a>
+				<h4>Cohort: CC{cohort}</h4>
+			</div>
 		</div>
 	);
 }

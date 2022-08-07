c09f2892852de0de520f31dfcfbaaf5bf5167edd
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-07 09:20:15 +0900

    Feat: Removed footer from login page to prevent unauthorised access

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import "../styles.css";
 import Login from "./Login";
 import Header from "./Header";
-import Footer from "./Footer";
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState("");
   return (
     <div>
-      <Header loggedInUser={loggedInUser}/>
+      <Header loggedInUser={loggedInUser} />
       <Login setLoggedInUser={(value) => setLoggedInUser(value)} />
-      <Footer />
     </div>
   );
 }

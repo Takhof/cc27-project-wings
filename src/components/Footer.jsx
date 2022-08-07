5136a4ee78122455ac8b43d6009e5a4cdf794d83
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-07 09:37:54 +0900

    Feat: Remove sign up and login links from footer when user has access to account

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,8 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-links-container">
-        <a href="/">Login</a>
-        <a href="/Register">Sign up</a>
         <a href="/CreateEditProfile">Create/Edit Profile</a>
-				<a href="/SingleProfileView">Single Profile View</a>
+        <a href="/SingleProfileView">Single Profile View</a>
         <a href="/Directory">Directory</a>
         <a href="/Contact"> Contact us</a>
         <small className="copyright"> Wings © 2022 Jozajo International </small>

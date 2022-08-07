8893679403d3cfea24b391eab89036e0271fcffb
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-08-07 09:28:15 +0900

    Chore: Import of CSS

--- src/Pages.js/Contact.jsx
+++ src/Pages.js/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import "../styles.css";
 function Contact() {
   return (
     <div>

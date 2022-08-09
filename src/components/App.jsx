c5211870c22ee15ea74be269ca7655ced01e9073
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-09 15:31:58 +0900

    feat: Add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,28 @@ import React, { useState } from "react";
 import "../styles.css";
 import Login from "./Login";
 import Header from "./Header";
+import useLocalStorage from 'use-local-storage'
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState("");
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
+
   return (
-    <div>
+      <div className = "App" data-theme = {theme}>
       <Header loggedInUser={loggedInUser} />
-      <Login setLoggedInUser={(value) => setLoggedInUser(value)} />
+      <button onClick = {switchTheme }>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+      </button>
+      <Login setLoggedInUser={(value) => setLoggedInUser(value)}
+      theme = {theme} />
+     
     </div>
   );
 }

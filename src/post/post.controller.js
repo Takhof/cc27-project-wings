0e6f878746f10fd87db1a47e4a06e9bf9c137464
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-08-09 16:36:50 +0900

    feat: Add controller for updating post

--- src/post/post.controller.js
+++ src/post/post.controller.js
@@ -1,21 +1,33 @@
 const postModel = require("./post.model");
 
 module.exports = {
-	index: async function (req, res) {
-		const posts = await postModel.getAll();
-		res.send(posts);
-	},
+  index: async function (req, res) {
+    const posts = await postModel.getAll();
+    res.send(posts);
+  },
 
-	save: async function (req, res) {
-		const { userId, text, photoURL } = req.body;
+  save: async function (req, res) {
+    const { userId, text, photoURL } = req.body;
 
-		const payload = {
-			user_id: userId,
-			post_text: text,
-			post_photo: photoURL,
-		};
+    const payload = {
+      user_id: userId,
+      post_text: text,
+      post_photo: photoURL,
+    };
 
-		const post = await postModel.create(payload);
-		res.send(post);
-	},
+    const post = await postModel.create(payload);
+    res.send(post);
+  },
+
+  updatePost: async function (req, res) {
+    try {
+      // TODO This needs to check the post that user going to update is actually belongs to the user
+      const { userId, postId, postText } = req.body;
+      postModel.updatePost({ postId, postText });
+      res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      res.status(500).send();
+    }
+  },
 };

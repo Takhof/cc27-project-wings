963524de1e1f8b016f6a1b536940e4487758d8e7
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-08-09 10:40:19 +0900

    feat: Add updated_at column to posts table

---
+++ db/migrations/20220809012051_add_updated_at_column_to_posts.js
@@ -0,0 +1,21 @@
+const TABLE_NAME = "posts";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dateTime("updated_at").notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropColumn("updated_at");
+  });
+};

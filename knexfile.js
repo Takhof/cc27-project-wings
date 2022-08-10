a592b838acfb2a3d1f4a25d9ea581da6063605e2
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-10 16:08:28 +0900

    chore: Add if condition to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  console.log("this was executed");
+  require("dotenv").config();
+}
 
 module.exports = {
   development: {
